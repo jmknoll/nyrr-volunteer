e338873c1176cb46bd2eb5620e0d4bfd25e3089b
Jameson Knoll <j.knoll621@gmail.com>
2023-07-26 12:52:06 -0400

    update

--- nyrr.py
+++ nyrr.py
@@ -10,13 +10,13 @@ class NYRRInterface:
         self.total_loaded = 8
         self.last_page = False
 
-        self.headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Max-Age": "3600",
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
-        }
+        # self.headers = {
+        #     "Access-Control-Allow-Origin": "*",
+        #     "Access-Control-Allow-Methods": "GET",
+        #     "Access-Control-Allow-Headers": "Content-Type",
+        #     "Access-Control-Max-Age": "3600",
+        #     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        # }
 
         self.url = "https://www.nyrr.org/api/feature/volunteer/FilterVolunteerOpportunities?available_only=true&itemId=3EB6F0CC-0D76-4BAF-A894-E2AB244CEB44&limit={limit}&offset={offset}&totalItemLoaded={total_loaded}"
 
@@ -25,8 +25,16 @@ class NYRRInterface:
         total_runs = 0
         descriptions = []
 
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        }
+
         while not self.last_page:
-            res = requests.get(self.url, headers=self.headers)
+            res = requests.get(self.url, headers)
             # probably check for errors here
             print(f"Response status code: {res.status_code}")
             print(f"Response headers: {res.headers}")
