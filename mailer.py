38811302184dc369f9128e4502e9339837916100
Jameson Knoll <j.knoll621@gmail.com>
2023-07-26 15:14:05 -0400

    add mailer class

--- mailer.py
+++ mailer.py
@@ -1,6 +1,28 @@
+import requests
+import json
+
+from util import Status
+from config import MAIL_API_KEY
+
+
 class Mailer:
     def __init__(self):
-        pass
+        self.url = "https://api.postmarkapp.com/email"
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-Postmark-Server-Token": MAIL_API_KEY,
+        }
 
-    def send(self):
-        pass
+    def send(self, from_email, to_email, subject, html_body, message_stream):
+        data = {
+            "From": from_email,
+            "To": to_email,
+            "Subject": subject,
+            "HtmlBody": html_body,
+            "MessageStream": message_stream,
+        }
+        res = requests.post(self.url, headers=self.headers, data=json.dumps(data))
+
+        data = res.json()
+        return data
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from nyrr import NYRRInterface
 from mailer import Mailer
 
 from util import Status
+from config import FROM_EMAIL, TO_EMAIL, MESSAGE_STREAM
 
 
 def main():
@@ -9,12 +10,30 @@ def main():
     mailer = Mailer()
 
     results = nyrr.find_opportunities()
-    if results.status == Status.SUCCESS:
-      print(results.message)
+
+    if results["status"] == Status.SUCCESS:
+        print(results["message"])
+
+        from_email = FROM_EMAIL
+        to_email = TO_EMAIL
+        subject = "[Automated] - NYRR Volunteer Opportunities"
+        body = format_list_as_html(results)
+        stream = MESSAGE_STREAM
+        # from, to, subject, body, stream
+        mail_response = mailer.send(from_email, to_email, subject, body, stream)
+        if mail_response["ErrorCode"] == 0:
+            print("mail success")
+        else:
+            print(f'mail error: {mail_response["Message"]}')
+
     # handle no results or error
-    else
-    
-    
+    else:
+        print(results["message"])
+
+
+def format_list_as_html(opps):
+    list_items = [f"<li>{opp}</li>" for opp in opps]
+    return f"<ul>{''.join(list_items)}</ul>"
 
 
 main()
